43e62b8e182d697b0717acd13b30b33bd3393cb1
C1one_38 <c1one@yandex.ru>
2016-01-30 17:52:27 +0300

    Full third homework

--- Lesson-3/js/main.js
+++ Lesson-3/js/main.js
@@ -7,7 +7,7 @@
     collection.eq(i).text('Замена №' + (i + 1));
   }
 
-}());
+})();
 
 // Задание № 2
 
@@ -16,7 +16,7 @@
   for (var i = 0 , len = collection.length; i < len; i++) {
     collection.eq(i).val('Замена №' + (i + 1));
   }
-}());
+})();
 
 
 // Задание № 3
@@ -24,13 +24,18 @@
 (function () {
   var collection = $('#third > select > [value="2"]');
   $('#third').append('<p>Найденный текст: ' + collection.eq(0).text() + '</p>');
-}());
+})();
 
 // Задание № 4
 
 (function () {
   var collection = $('#fourth > ul > li:nth-child(2)');
   collection.eq(0).wrapInner('<b></b>');
-}());
+})();
 
 // Задание № 5
+
+(function () {
+  var collection = $('#fourth > ul > li:last-child');
+  $('#fifth').append(collection.eq(0));
+})();
